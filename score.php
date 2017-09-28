00de7caf0ff67fc23409d7ce3883f2c2be53e824
Jasper Lance Dayon <jasperlancecdayon@yahoo.com>
2017-09-28 16:22:53 +0800

    Completed score insert wth a data based on the username in GrooveShot

--- score.php
+++ score.php
@@ -1,8 +1,8 @@
 <?php
 include 'dbconn.php';
 
-$username = 'SkildX';
-$score = '38';
+$username = $_POST["usernameHere"];
+$score = $_POST["scoreHere"];
 
 //Find user for user ID
 $q_userID = mysqli_query($conn,"SELECT `id` FROM `users` WHERE `username` = '$username'");
@@ -12,21 +12,29 @@ $id_row = mysqli_fetch_assoc($q_userID);
 $userid = $id_row["id"];
 
 //check if user is found
-if($q_userID){
+if($q_userID)
+{
     echo "User ID found!<br/>";
-}else{
-    die("Failed<br/>");
-}
 
-
-
-//Insert score record
-$insert = "INSERT INTO `score_board`(`userID`,`username`,`score`) VALUES ('$userid','$username','$score')";
-$q_scoreRecord = mysqli_query($conn,$insert);
+    //Insert score record
+	$insert = "INSERT INTO `score_board`(`userID`,`username`,`score`) VALUES ('$userid','$username','$score')";
+	$q_scoreRecord = mysqli_query($conn,$insert);
 
 //check if query is successful
-if($q_scoreRecord){
+if($q_scoreRecord)
+{
     echo "Score Record inserted<br/>";
-}else{
+}
+else
+{
     die("Failed<br/>");
 }
+
+}
+else
+{
+    die("Failed<br/>");
+}
+
+
+
